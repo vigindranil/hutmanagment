21b7747500544cabf639dec838dffea156277825
Hritabrata-Vyoma <hritabrata.d@vyomainnovusglobal.com>
2026-02-23 14:31:36 +0530

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/haatmanagement',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
       '/HMSRestAPI': {
-        target: 'https://vigpl.com',
+        target: 'https://haatmgmtjpgzp.wb.gov.in',
         changeOrigin: true,
         secure: false,
       },
